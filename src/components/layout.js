a5040999feaafcf13e8640c2285288322c349c11
Mike Mathew <m2mathew@me.com>
2019-06-19 18:03:24 -0500

    Updates layout to use makeStyles

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,47 +5,58 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+ // External Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
 import {
   StaticQuery,
   graphql,
-} from "gatsby"
+} from 'gatsby';
+import { makeStyles } from '@material-ui/styles';
 
-import Header from "./header"
-import "./layout.css"
+// Internal Dependencies
+import Header from './header';
+import './layout.css';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+// Local Variables
+const useStyles = makeStyles({
+  container: {
+    margin: '4.5em auto 0 auto',
+    maxWidth: 960,
+    padding: `0px 1.0875rem 1.45rem`,
+    paddingTop: 0,
+  },
+});
+
+// Component Definition
+const Layout = (props) => {
+  const { children } = props;
+  const classes = useStyles(props);
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '4.5em auto 0 auto',
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}{' '}Mike Mathew
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+      `}
+      render={data => (
+        <>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div className={classes.container}>
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}{' '}Mike Mathew
+            </footer>
+          </div>
+        </>
+      )}
+    />
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
