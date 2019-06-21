8b3dd27fe45ddfad5c845ab16a91e475158f83fb
Mike Mathew <m2mathew@me.com>
2019-06-20 21:24:51 -0500

    Updates header for screen sizes, adds subtitle

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,28 @@ import { makeStyles } from '@material-ui/styles';
 
 // Local Variables
 const useStyles = makeStyles(theme => ({
+  root: {
+    alignItems: 'flex-end',
+    display: 'flex',
+    margin: '1.5rem 0',
+    flexDirection: 'column',
+  },
+  subtitle: {
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 14,
+    },
+    [theme.breakpoints.up('sm')]: {
+      fontSize: 18,
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: 22,
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: 28,
+    },
+    alignSelf: 'center',
+    marginTop: 12,
+  },
   title: {
     [theme.breakpoints.down('xs')]: {
       fontSize: 30,
@@ -31,12 +53,19 @@ const useStyles = makeStyles(theme => ({
 const Header = (props) => {
   const classes = useStyles(props);
   return (
-    <div>
+    <div className={classes.root}>
       <Typography
         className={classes.title}
         variant="h1"
       >
         Mike Mathew
+      </Typography>
+      <Typography
+        className={classes.subtitle}
+        component="h3"
+        variant="h5"
+      >
+        front end web developer
       </Typography>
     </div>
   );
