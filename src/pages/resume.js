1a117e696d479f593801f1fa729a96b428ae9c8c
Mike Mathew <m2mathew@me.com>
2019-06-22 00:44:20 -0500

    Adds resume-section component

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/styles';
 // Internal Dependencies
 import Header from '../components/header';
 import SEO from '../components/seo';
+import ResumeSection from '../components/resume-section';
 import Subtitle from '../components/subtitle';
 
 // Component Definition
@@ -14,6 +15,11 @@ const Resume = () => (
     <SEO title="Resume" />
     <Header />
     <Subtitle>Résumé</Subtitle>
+    <ResumeSection>Summary</ResumeSection>
+    <ResumeSection>Specialties</ResumeSection>
+    <ResumeSection>Experience</ResumeSection>
+    <ResumeSection>Education</ResumeSection>
+    <ResumeSection>Projects</ResumeSection>
   </>
 );
 
