352b72e9877bc8889861d6689b9a91c2ae5f570c
Mike Mathew <m2mathew@me.com>
2019-06-21 22:16:25 -0500

    Add a nice svg swoop in the header

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,6 @@ import { bioPhrases } from '../utils/constants/home-constants';
 const useStyles = makeStyles(theme => ({
   bioContainer: {
     borderBottom: `10px solid ${theme.palette.grey[500]}`,
-    borderTop: `10px solid ${theme.palette.grey[500]}`,
     lineHeight: 3,
     padding: '16px 0',
     textAlign: 'justify',
@@ -44,8 +43,11 @@ const IndexPage = (props) => {
       <SEO title="Home" />
       <Header />
       <div>
-        <svg viewBox="0 0 1440 120">
-          <path d="M1440,21.2101911 L1440,120 L0,120 L0,21.2101911 C120,35.0700637 240,42 360,42 C480,42 600,35.0700637 720,21.2101911 C808.32779,12.416393 874.573633,6.87702029 918.737528,4.59207306 C972.491685,1.8109458 1026.24584,0.420382166 1080,0.420382166 C1200,0.420382166 1320,7.35031847 1440,21.2101911 Z" />
+        <svg viewBox="0 0 1440 64" width="100%">
+          <path
+            fill="rebeccapurple"
+            d="M1440,21.2101911 L1440,120 L0,120 L0,21.2101911 C120,35.0700637 240,42 360,42 C480,42 600,35.0700637 720,21.2101911 C808.32779,12.416393 874.573633,6.87702029 918.737528,4.59207306 C972.491685,1.8109458 1026.24584,0.420382166 1080,0.420382166 C1200,0.420382166 1320,7.35031847 1440,21.2101911 Z"
+          />
         </svg>
       </div>
       <div className={classes.bioContainer}>
